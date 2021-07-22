4611a01770d1176a500b581697ccd3dc5241e440
nearnshaw <nearnshaw@decentraland.org>
2021-07-22 17:50:08 -0300

    CW-NW

--- src/game.ts
+++ src/game.ts
@@ -8,36 +8,17 @@ Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (e) => {
   log(`rot: `, Camera.instance.rotation)
 })
 
-let sceneOrientation: RoadOrientation = RoadOrientation.horizontal
+let sceneOrientation: RoadOrientation = RoadOrientation.vertical
 
-let trackLength: number = 97
+let trackLength: number = 47
 
 let stationCount: number = 3
 
 let cycleTime: number = 250 //180 //(56 / 180) * trackLength
 
 let exceptions: RoadType[] = []
-
-exceptions[6] = RoadType.ROADLEFT
-
-// exceptions[15] = RoadType.ROADBOTH
-// exceptions[11] = RoadType.WROADRIGHT2
-
-// exceptions[21] = RoadType.WROADBOTH1
-// exceptions[22] = RoadType.WROADBOTH2
-
-// exceptions[32] = RoadType.STATION
-// exceptions[33] = RoadType.STATION
-// exceptions[34] = RoadType.STATION
-
-exceptions[50] = RoadType.WROADBOTH1
-exceptions[51] = RoadType.WROADBOTH2
-
-// exceptions[53] = RoadType.ROADRIGHT
-// exceptions[56] = RoadType.ROADRIGHT
-// exceptions[59] = RoadType.ROADRIGHT
-
-setTramType(TranType.forest)
+exceptions[44] =RoadType.ROADRIGHT
+setTramType(TranType.pop)
 
 addRoad(trackLength + 2, sceneOrientation, exceptions)
 
